5283c3ade4a607a638c218650d68185506fdb9e6
SMCIND <smritichangulani@gmail.com>
2018-02-22 01:06:38 +0530

    Update Q3_NER.py

--- 201711012_JivaniForam/Assignment3/Q3_NER.py
+++ 201711012_JivaniForam/Assignment3/Q3_NER.py
@@ -1,31 +1,29 @@
-# -*- coding: utf-8 -*-
-
 import spacy
 import en_core_web_sm
 import pandas as pd
 nlp = en_core_web_sm.load()
 
-df = pd.read_csv('C:\\Users\\student\\Downloads\\BXCSVDump\\BX-Books2.csv')
-#df['text_as_spacy_title'] = df['Book-Title'].apply(nlp)
-print(df['Book-Title'])
-
-'''
-doc = nlp('am is are eat ate eaten car cars car''s cars'' god''s ')
-
-for token in doc:
-        print(token.text, token.lemma_)
-'''
-        
+df = pd.read_csv('C:\\Users\\dell pc\\Desktop\\Project\\BX-BookData_new.csv')
+print("\n        For BOOK TITLE    \n")
+     
 for item in df['Book-Title']:
-    #item.replace("'","''")
-   # print(item)
+    doc = nlp(item)
+    #print(doc)
+    for ent in doc.ents:
+        print(ent.text,  '|',ent.label_)
+        
+print("\n        For PUBLISHERS    \n")  
+
+for item in df['Publisher']:
     doc = nlp(item)
     #print(doc)
     for ent in doc.ents:
         print(ent.text,  '|',ent.label_)
 
-'''
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-          token.shape_, token.is_alpha, token.is_stop)
-'''
+print("\n        For GENRE    \n")
+
+for item in df['Genre']:
+    doc = nlp(item)
+    #print(doc)
+    for ent in doc.ents:
+        print(ent.text,  '|',ent.label_)
