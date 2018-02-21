1f39e475a5778cd141e0a89d07740f886247f0a7
SMCIND <smritichangulani@gmail.com>
2018-02-22 01:00:53 +0530

    Update Q2_ Lemmatization.py

--- 201711056_MausameePatel/Assignment3/Q2_ Lemmatization.py
+++ 201711056_MausameePatel/Assignment3/Q2_ Lemmatization.py
@@ -1,28 +1,27 @@
-
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import spacy
 import en_core_web_sm
 import pandas as pd
+
 nlp = en_core_web_sm.load()
 
-df= pd.read_csv('C:\\Users\\student\\Desktop\\mausamee\\bx-books-test-1.csv')
+df= pd.read_csv('C:\\Users\\dell pc\\Desktop\\Project\\BX-BookData_new.csv')
 
-for token in doc:
-    print(token.text, token.lemma_)
-
+print("\n        For BOOK TITLE    \n")
 for item in df['Book-Title']:
     doc=nlp(item)
     #print(doc)
     for token in doc:
         print(token.text, token.lemma_)
 
-
+print("\n        For PUBLISHERS    \n")  
 for item1 in df['Publisher']:
+    doc=nlp(item1)
+    #print(doc)
+    for token in doc:
+        print(token.text, token.lemma_)
+
+print("\n        For GENRE    \n")
+for item1 in df['Genre']:
     doc=nlp(item1)
     #print(doc)
     for token in doc:
