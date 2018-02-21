1834518b6ffc75430c5a54233b6527c369bd1454
SMCIND <smritichangulani@gmail.com>
2018-02-22 00:47:09 +0530

    Update Q1_NounPhraseExtraction.py

--- 201711019_Smriti_Changulani/Assignment3/Q1_NounPhraseExtraction.py
+++ 201711019_Smriti_Changulani/Assignment3/Q1_NounPhraseExtraction.py
@@ -1,19 +1,25 @@
 import spacy
 import en_core_web_sm
-import csv
 import pandas as pd
 
 nlp = en_core_web_sm.load()
 
-
-
-df = pd.read_csv('bx-books-test-1.csv')
+df = pd.read_csv('C:\\Users\\dell pc\\Desktop\\Project\\BX-BookData_new.csv')
 df1 = df['Book-Title'].apply(nlp)
-#print(df['text_spacy_title'])
-
 for temp in df1:
         for chunk in temp.noun_chunks:
           print(chunk.root.text)
+print("\n")
+          
+df2 = df['Publisher'].apply(nlp)
+for temp in df2:
+        for chunk in temp.noun_chunks:
+          print(chunk.root.text)
+print("\n")
 
-        print("\n")
+df3 = df['Genre'].apply(nlp)
+for temp in df3:
+        for chunk in temp.noun_chunks:
+          print(chunk.root.text)
+
           
